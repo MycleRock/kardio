af9baa527b61614fa28dff06a031ef5dadca7fe6
Mycle Rock <info@krelle-informatik.de>
2024-08-08 09:53:21 +0200

    08-24

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitdde61db2dc32a7876d16ff8608905779::getLoader();
+return ComposerAutoloaderInit7699f031d1d6c50ede450f58e2ed6b8e::getLoader();
